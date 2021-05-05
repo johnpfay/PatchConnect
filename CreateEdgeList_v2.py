0dbe0c8d4dc25e8faf0d3b695d740430c891d9ee
John Fay <john.fay@duke.edu>
2021-05-05 09:32:57 -0400

    Fixed conversion of image coords to geographic

--- CreateEdgeList_v2.py
+++ CreateEdgeList_v2.py
@@ -14,46 +14,66 @@ Spring 2021 - John.Fay@duke.edu
 '''
 
 
-#%%Import libraries
+#%% SET UP
+
+#Import libraries
 import arcpy
 import numpy as np
+import pandas as pd
 from skimage import graph
+from arcgis import GIS, GeoAccessor, geometry
 
-arcpy.env.overwriteOutput = True
+#Initialize the ArcGIS API "GIS" object
+gis = GIS('home')
 
-#%% Get input datasets: Patches and CostSurface
+# Get input datasets: Patches and CostSurface
 debug = True
 if debug:
     patchRaster = '..\\Data\\ENH_LCP_ModelInputs_Final2019.gdb\\S_Patches_60ha'
     orig_costRaster = '..\\Data\\ENH_LCP_ModelInputs_Final2019.gdb\\S_CostSurface'
-    edgeListFN = '..\\Scratch\\edgelist.csv'
+    edgeListFN = '..\\Scratch\\edgelist4.csv'
+    lcp_featureclass = edgeListFN.replace('.csv','.shp')
+    arcpy.env.overwriteOutput = True
+    calc_lcps = True
 else:
     patchRaster = arcpy.GetParameterAsText(0)
     orig_costRaster = arcpy.GetParameterAsText(1)
     edgeListFN = arcpy.GetParameterAsText(2)
+    lcp_featureclass = edgeListFN.replace('.csv','.shp')
+    arcpy.SetParameterAsText(3,lcp_featureclass)
+
+# Subset the cost raster to match dimensions of the patch raster
+arcpy.env.cellSize = patchRaster
+arcpy.env.extent = patchRaster
+arcpy.env.snapRaster = patchRaster
 
 
-#%% Message function
+# Get the spatial reference, extent, and lower left coordinates
+sr = arcpy.Describe(patchRaster).spatialReference
+cellSize = arcpy.Describe(patchRaster).meanCellWidth
+extent = arcpy.Describe(patchRaster).extent
+llCorner = extent.lowerLeft
+
+#%% FUNCTIONS
+
+#Message function: shows messages locally and in ArcGIS Pro
 def msg(txt):
     print(txt)
     arcpy.AddMessage(txt)
     return
 
-#%% Subset the cost raster to match dimensions of the patch raster
-arcpy.env.cellSize = patchRaster
-arcpy.env.extent = patchRaster
-arcpy.env.snapRaster = patchRaster
+#Convert row-column values to geographic coordinates
+def to_xy(row,col):
+    x_geom = extent.upperLeft.X + (col * cellSize) 
+    y_geom = extent.upperLeft.Y - (row * cellSize) 
+    return [x_geom,y_geom]
 
+#%% Subset the cost raster to match the patch raster
 msg("Subsetting cost raster")
 costRaster = arcpy.sa.ExtractByRectangle(orig_costRaster, patchRaster, "INSIDE")
 
-#%% Get the spatial reference, extent, and lower left coordinates
-sr = arcpy.Describe(costRaster).spatialReference
-cellSize = arcpy.Describe(costRaster).meanCellWidth
-extent = arcpy.Describe(costRaster).extent
-llCorner = extent.lowerLeft
-
 #%% Create arrays of the patch and core rasters
+msg("Converting rasters to NumPy arrays")
 arrPatch = arcpy.RasterToNumPyArray(patchRaster,
                                     lower_left_corner=llCorner,
                                     nodata_to_value=-9999)
@@ -65,56 +85,100 @@ arrCost = arcpy.RasterToNumPyArray(costRaster,
 #%% Create a list of patchIDs
 patchIDs = np.unique(arrPatch).tolist()
 patchIDs.remove(-9999)
+msg(f"{len(patchIDs)} patches to process")
+
+#%% Initialize the output dataframe and CostDistArrays list
+df_patches = pd.DataFrame(columns = ['FROM_ID','TO_ID','COST','geometry'])
+costDistArrays = []
 
 #%% Initialize the progressor
 step = 0
 steps = len(patchIDs)
 arcpy.SetProgressor("step", "Computing Cost Distances...",step,steps,1)
 
-#%% Initialize the arrayList and edgeList lists
-arrList = []
-tracebackList = []
-edgeList = []
-
 #%% Loop through each patch and compute its cost distance to all other patches
 for patchID in patchIDs[:1]:
-    arcpy.SetProgressorLabel("Patch {} of {}".format(patchID,steps))
+    arcpy.SetProgressorLabel("Patch {} of {} ".format(patchID,steps))
+    print("\nPatch {} of {}".format(patchID,steps),end='')
 
-    #Reclassify cost in source patch cells to zero
+    #Reclassify cost in source patch cells to zero & set no data to high cost
     arrCostMod = arrCost.copy()
     arrCostMod[arrPatch == patchID] = 0
-    arrCostMod[arrCostMod == -9999] = 100000
+    #arrCostMod[arrCostMod == -9999] = arrCostMod.max() * 10000
 
     #Create the MCP object (Geometric accounts for diagonals)
-    lg = graph.MCP_Geometric(arrCostMod, sampling=(cellSize, cellSize))
+    cost_graph = graph.MCP_Geometric(arrCostMod, sampling=(cellSize, cellSize))
 
     #Get the index of a cell in the current patch ID
     i,j = np.where(arrPatch == patchID)
     startCells = list(zip(i,j))
 
     #Compute cost distance and traceback arrays from a source
-    cd_array, tb_array = lg.find_costs(starts=startCells)
+    cd_array = cost_graph.find_costs(starts=startCells)[0]
     
-#Write the output to the edgelist
+    #Process all the to-patches
     for toID in patchIDs:
         if toID > patchID:
-            edgeList.append((patchID, toID, cd_array[arrPatch == toID].min()))
-
+            print(".",end="")
+            #--Extract the cost distance between patches--
+            least_cost_distance = cd_array[arrPatch == toID].min()
+            
+            #--Skip if no LCP was found
+            if least_cost_distance == float('inf'): continue
+            
+            #--Compute the least cost path polyline--
+            #Step 1. Find the cell[s] with the lowest cost
+            rowMin,colMin = np.where(cd_array == least_cost_distance)
+            
+            ###---IF LCP FEATURES ARE REQUESTED---
+            if calc_lcps:
+                #Step 2. Compute the least cost path (traceback) from this cell
+                lcp_coords = cost_graph.traceback((rowMin[0],colMin[0]))
+                #Step 3. Convert image coords to geographic coords
+                lcp_coords_geog = [to_xy(r,c) for r,c in lcp_coords]
+                #Step 4. Construct a linestring from the coordinates
+                the_linestring = {"paths":[lcp_coords_geog],
+                                  "spatialReference":{"wkid":sr.factoryCode}}
+                #Step 5. Construct a PolyLine geometry from the linestring
+                the_polyline = geometry.Polyline(the_linestring)
+                
+                #--Add the data to the dataframe
+                df_patches = df_patches.append({
+                    'FROM_ID': patchID, 
+                    'TO_ID': toID,
+                    'COST': least_cost_distance,
+                    'geometry': the_polyline},ignore_index=True)
+            else:
+                #--Add the data to the dataframe,w/o geometry
+                df_patches = df_patches.append({
+                    'FROM_ID': patchID, 
+                    'TO_ID': toID,
+                    'COST': least_cost_distance},ignore_index=True)
+            
     #Add arrays to arrLists
-    arrList.append(cd_array)
+    costDistArrays.append(cd_array)
     arcpy.SetProgressorPosition()
 
+#%% Clean up
+
+#Convert dataframe to spatial dataframe
+if calc_lcps:
+    arcpy.SetProgressor("default","Converting features to a spatial dataframe")
+    print("Converting to spatial dataframe")
+    sdf_patches = GeoAccessor.from_df(df_patches, geometry_column='geometry')
+    
+    #Save as a feature class
+    arcpy.SetProgressor("default",f"Saving least cost paths to {lcp_featureclass}")
+    print(f"Saving least cost paths to {lcp_featureclass}")
+    sdf_patches.spatial.to_featureclass(lcp_featureclass)
+
 #Write the edges to the edgeListFN
-arcpy.SetProgressor("default", "Saving Edges to {}".format(edgeListFN))
-np.savetxt(edgeListFN,np.asarray(edgeList),
-           comments='',
-           delimiter=",", 
-           fmt='%d,%d,%2.4f', 
-           header=("From,To,Cost"))
+arcpy.SetProgressor("default", f"Saving Edges to {edgeListFN}")
+df_patches[['FROM_ID','TO_ID','COST']].to_csv(edgeListFN,float_format=("%2.4f"),index=False)
 
 #Write out cost surface arrays
-arcpy.SetProgressor("default", "Stacking arrays")
-arrStack = np.stack(arrList)
+# arcpy.SetProgressor("default", "Stacking arrays")
+# arrStack = np.stack(costDistArrays)
 
-arcpy.SetProgressor("default", "Saving Cost Distance Arrays to {}".format(edgeListFN))
-np.save(edgeListFN.replace("csv","npy"),arrStack)
+# arcpy.SetProgressor("default", "Saving Cost Distance Arrays to {}".format(edgeListFN))
+# np.save(edgeListFN.replace("csv","npy"),arrStack)
