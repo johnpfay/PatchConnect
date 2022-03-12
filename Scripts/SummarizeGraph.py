938dc4049f892c822b186b8efb3dcbb8c65c219a
John Fay <john.fay@duke.edu>
2022-03-12 13:17:15 -0500

    Update for NetworkX vs 2.x

--- Scripts/SummarizeGraph.py
+++ Scripts/SummarizeGraph.py
@@ -116,7 +116,9 @@ def graph_comp_sequence(Gts):
            diam = x_diameter(g)
        else:
            nc = nx.number_connected_components(g)
-           gc = max(nx.connected_component_subgraphs(g), key=len)# nx.connected_component_subgraphs(g)[0]
+           #gc = max(nx.connected_component_subgraphs(g), key=len)# nx.connected_component_subgraphs(g)[0]
+           gc_set = sorted(nx.connected_components(g), key=len, reverse=True)
+           gc = G.subgraph(gc_set[0])
            diam = x_diameter(gc)
        gcs[d] = (nc, diam)
        if start_diam * 0.9 > diam:
